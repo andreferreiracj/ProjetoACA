dd80685cda3e565c7376d4b7a934ab8dd54f6a8d
André Ferreira <65228397+andreferreiracj@users.noreply.github.com>
2021-01-12 12:56:56 +0000

    Add files via upload

---
+++ film.php
@@ -0,0 +1,66 @@
+<?php
+class film {
+    //ATRIBUTOS
+    public  $title;
+    public  $description;
+    public  $genre;
+    public  $director;
+    public $link;
+    
+
+    //========================GETS E SETS==============================
+    //get e set do titulo
+    function setTitle( $title) {
+        $this->title = $title;
+      }
+      function getTitle() {
+        return $this->title;
+      }
+
+      //get e set da descrição
+      function setDescription($description) {
+        $this->description = $description;
+      }
+      function getDescription() {
+        return $this->description;
+      }
+
+      //get e set do director
+      function setDirector($director) {
+        $this->director = $director;
+      }
+      function getDirector() {
+        return $this->director;
+      }
+     
+      //get e set do link
+      function setLink($link) {
+        $this->link = $link;
+      }
+      function getlink() {
+        return $this->link;
+      }
+
+      //=====================Construtores================================
+      function __construct($title,$description,$genre,$director) {
+        $this->title = $title;
+        $this->description = $description;
+        $this->genre = $genre;
+        $this->director = $director;
+        $this->link = $link;
+      }
+    
+      //output
+      public function __toString(){
+        return  
+        ' =================Movie================= ' ."\r\n".
+        ' Title: ' . $this->title  . "\r\n" . 
+        ' Description: ' . $this->description  . "\r\n" .
+        ' Genre: ' . $this->genre . "\r\n" .
+        ' Director: ' . $this->director . "\r\n" .
+        ' Imdb link: ' . $this->link;
+
+       
+    }
+     
+}
