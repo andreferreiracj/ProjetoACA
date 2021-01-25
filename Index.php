73681a4ef9dcceebb1d79c54c21e0284cf3a80dc
André Ferreira <65228397+andreferreiracj@users.noreply.github.com>
2021-01-25 13:37:57 +0000

    Update Index.php

--- Index.php
+++ Index.php
@@ -18,17 +18,16 @@ $discordBot->setLoopInterface($loop);
 
 //============================funcoes do discord================================================
 $discordBot->on('event.MESSAGE_CREATE', function( DiscordClient $discordBot, int $shard, String $event, Array $data) {
-  
 
- if(strtolower($data['content']) == '!horror') {
+if(strtolower($data['content']) == '!horror') {
     $film1 = new film("Hereditary","A grieving family is haunted by tragic and disturbing occurrences.",
-    "horror","Ari Aster");
+    "horror","Ari Aster","https://www.imdb.com/title/tt7784604/");
      $film2 = new film("Midsommar","A couple travels to Eastern Europe to visit a rural hometown's fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.",
-     "horror","Ari Aster");
+     "horror","Ari Aster","https://www.imdb.com/title/tt8772262/?ref_=nv_sr_srsg_0");
      $film3 = new film("Exorcist","When a 12-year-old girl is possessed by a mysterious entity, her mother seeks the help of two priests to save her.",
-     "horror","William Friedkin");
+     "horror","William Friedkin","https://www.imdb.com/title/tt0070047/?ref_=fn_al_tt_1");
      $film4 = new film("Halloween","Fifteen years after murdering his sister on Halloween night 1963, Michael Myers escapes from a mental hospital and returns to the small town of Haddonfield, Illinois to kill again.",
-     "horror","John Carpenter");
+     "horror","John Carpenter","https://www.imdb.com/title/tt0077651/?ref_=fn_al_tt_1");
     
     $horrorFilms= array($film1,$film2,$film3,$film4);
     $response= array_rand($horrorFilms);
@@ -37,13 +36,13 @@ $discordBot->on('event.MESSAGE_CREATE', function( DiscordClient $discordBot, int
 	
 if(strtolower($data['content']) == '!drama') {
   $film5 = new film("Another Round","Four friends, all high school teachers, test a theory that they will improve their lives by maintaining a constant level of alcohol in their blood.",
-  "drama","Thomas Vinterberg");
+  "drama","Thomas Vinterberg","https://www.imdb.com/title/tt10288566/");
   $film6 = new film("HackSaw Ridge","World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to receive the Medal of Honor without firing a shot.",
-  "drama","Mel Gibson");
+  "drama","Mel Gibson","https://www.imdb.com/title/tt2119532/?ref_=nv_sr_srsg_0");
   $film7 = new film("Never Rarely Sometimes Always","A pair of teenage girls in rural Pennsylvania travel to New York City to seek out medical help after an unintended pregnancy.",
-  "drama","Eliza Hittman");
+  "drama","Eliza Hittman","https://www.imdb.com/title/tt7772582/?ref_=fn_al_tt_1");
   $film8 = new film("Good Will Hunting","Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.",
-  "drama","Gus Van Sant");
+  "drama","Gus Van Sant","https://www.imdb.com/title/tt0119217/?ref_=fn_al_tt_1");
   $dramaFilms= array($film5,$film6,$film7,$film8);
   $response2= array_rand($dramaFilms);
   $discordBot->sendChannelMessage($data['guild_id'], $data['channel_id'], $dramaFilms[$response2]->__toString() . "\r\n" . ' BO HOO stop crying!');
@@ -53,11 +52,11 @@ if(strtolower($data['content']) == '!action') {
   $film9 = new film("Bad Boys","Two hip detectives protect a witness to a murder while investigating a case of stolen heroin from the evidence storage room from their police precinct.",
    "action","Michael Bay");
    $film10 = new film("American Ultra","A stoner - who is in fact a government agent - is marked as a liability and targeted for extermination. But he's too well-trained and too high for them to handle.",
-   "action","Nima Nourizadeh");
+   "action","Nima Nourizadeh","https://www.imdb.com/title/tt3316948/?ref_=fn_al_tt_1");
    $film11 = new film("Tenet","Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
-   "action","Christopher Nolan");
+   "action","Christopher Nolan","https://www.imdb.com/title/tt6723592/?ref_=fn_al_tt_1");
    $film12 = new film("The Matrix","When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
-   "action","Lana Wachowski and Lilly Wachowski ");
+   "action","Lana Wachowski and Lilly Wachowski ","https://www.imdb.com/title/tt0133093/?ref_=nv_sr_srsg_2");
    $actionFilms= array($film9,$film10,$film11,$film12);
    $response3= array_rand($actionFilms);
    $discordBot->sendChannelMessage($data['guild_id'], $data['channel_id'], $actionFilms[$response3]->__toString() . "\r\n" . 'pew pew');
@@ -65,13 +64,13 @@ if(strtolower($data['content']) == '!action') {
 	
 if(strtolower($data['content']) == '!thriller') {
   $film13 = new film("Nightcrawler","When Louis Bloom, a con man desperate for work, muscles into the world of L.A. crime journalism, he blurs the line between observer and participant to become the star of his own story",
-  "Thriller","Dan Gilroy");
+  "Thriller","Dan Gilroy","https://www.imdb.com/title/tt2872718/?ref_=nv_sr_srsg_0");
   $film14 = new film("Parasite","Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-  "Thriller","Bong Joon Ho");
+  "Thriller","Bong Joon Ho","https://www.imdb.com/title/tt6751668/?ref_=nv_sr_srsg_0");
   $film15 = new film("Fight Club","An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
-  "Thriller","David Fincher");
+  "Thriller","David Fincher","https://www.imdb.com/title/tt0137523/?ref_=nv_sr_srsg_0");
   $film16 = new film("The Devil All the Time","Sinister characters converge around a young man devoted to protecting those he loves in a postwar backwoods town teeming with corruption and brutality.",
-  "Thriller","Antonio Campos");
+  "Thriller","Antonio Campos","https://www.imdb.com/title/tt7395114/?ref_=fn_al_tt_1");
   $thrillerFilms= array($film13,$film14,$film15,$film16);
    $response4= array_rand($thrillerFilms);
   $discordBot->sendChannelMessage($data['guild_id'], $data['channel_id'], $thrillerFilms[$response4]->__toString() . "\r\n" . 'hmmmmm interesting and misterious hun');
@@ -79,26 +78,35 @@ if(strtolower($data['content']) == '!thriller') {
 
 if(strtolower($data['content']) == '!comedy') {
   $film17 = new film("Death at a funeral","Chaos ensues when a man tries to expose a dark secret regarding a recently deceased patriarch of a dysfunctional British family.",
-  "comedy","Frank Oz");
+  "comedy","Frank Oz","https://www.imdb.com/title/tt0795368/?ref_=nv_sr_srsg_0");
   $film18 = new film("Scott Pilgrim vs. the World ","In a magically realistic version of Toronto, a young man must defeat his new girlfriend's seven evil exes one by one in order to win her heart.",
-  "comedy","Edgar Wright");
+  "comedy","Edgar Wright","https://www.imdb.com/title/tt0446029/?ref_=nv_sr_srsg_0");
   $film19 = new film("21 Jump Street","A pair of underachieving cops are sent back to a local high school to blend in and bring down a synthetic drug ring.",
-  "comedy","Phil Lord and Christopher Miller");
+  "comedy","Phil Lord and Christopher Miller","https://www.imdb.com/title/tt1232829/");
   $film20 = new film("Ted","John Bennett, a man whose childhood wish of bringing his teddy bear to life came true, now must decide between keeping the relationship with the bear, Ted or his girlfriend, Lori.",
-  "comedy","Seth MacFarlane");
+  "comedy","Seth MacFarlane","https://www.imdb.com/title/tt1637725/?ref_=fn_al_tt_1");
   $comedyFilms= array($film17,$film18,$film19,$film20);
   $response5= array_rand($comedyFilms);
   $discordBot->sendChannelMessage($data['guild_id'], $data['channel_id'], $comedyFilms[$response5]->__toString() . "\r\n" . 'haha very funny');
         }
+
 if(strtolower($data['content']) == '!noticias') {
   $bot = new bot ();
   $bot->devolverHtml(noticia);
   $discordBot->sendChannelMessage($data['guild_id'], $data['channel_id'], $bot->receberTags() . "\r\n" . 'Latest news');
-         }	
+         }
+
+
+
 });
 
 $discordBot->connect();
 $loop->run();
+
+
+
+
+
 
 
 
